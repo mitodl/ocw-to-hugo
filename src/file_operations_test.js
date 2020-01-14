613575223f1e43fa15811249e21a9fc9a4162bcc
Carey Gumaer <gumaerc@mit.edu>
2020-01-14 11:02:19 -0500

    Fix pathing to file_operations module and add a test for making sure the proper amount of directories are found.

--- src/file_operations_test.js
+++ src/file_operations_test.js
@@ -1,17 +1,19 @@
 const assert = require("assert")
 const expect = require("expect.js")
-const { scanCourses } = require("../lib/file_operations")
+const { scanCourses } = require("./file_operations")
 const fs = require("fs")
 const sinon = require("sinon")
 
 describe("scan_courses", () => {
-  let readdir
+  let readdir, consoleLog
 
   beforeEach(() => {
     readdir = sinon.stub(fs, "readdir").returns({})
+    consoleLog = sinon.stub(console, "log").returns({})
   })
   afterEach(() => {
     readdir.restore()
+    consoleLog.restore()
   })
 
   it("throws an error when you call it with no source directory", () => {
@@ -29,5 +31,10 @@ describe("scan_courses", () => {
   it("calls readdir once", () => {
     scanCourses("test_data/source", "test_data/destination")
     expect(readdir.calledOnceWith("test_data/source")).to.be(true)
+  })
+
+  it("scans the three test courses and reports to console", () => {
+    scanCourses("test_data/source", "test_data/destination")
+    expect(consoleLog.calledOnceWith("Scanning 3 subdirectories under test_data/source"))
   })
 })
