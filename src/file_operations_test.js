b2e7f521f13df31dd86baba33ded7bdd3af8fbb9
Carey Gumaer <gumaer.carey@gmail.com>
2020-06-17 17:10:28 -0400

    fmt

--- src/file_operations_test.js
+++ src/file_operations_test.js
@@ -70,7 +70,11 @@ describe("scanCourses", () => {
   })
 
   it("displays an error when you call it with an empty courses.json", () => {
-    fileOperations.scanCourses(inputPath, outputPath, "test_data/courses_blank.json")
+    fileOperations.scanCourses(
+      inputPath,
+      outputPath,
+      "test_data/courses_blank.json"
+    )
     expect(consoleLog).calledWithExactly(NO_COURSES_FOUND_MESSAGE)
   })
 
