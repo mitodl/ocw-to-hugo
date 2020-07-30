e51edd37cf9b42797c1a2e67479ba1d1050e023c
Carey P Gumaer <gumaerc@mit.edu>
2020-07-30 16:11:59 -0400

    resolve course uid's (#53)
    * refactor getting the master json filename into its own function and add code to generate a map of course id -> course uid
    
    * linting and formatting
    
    * remove extra console.log
    
    * structure getMasterJsonFileName for better error handling and add tests
    
    * fmt

--- src/file_operations.js
+++ src/file_operations.js
@@ -9,7 +9,7 @@ const {
   MISSING_COURSE_ERROR_MESSAGE,
   NO_COURSES_FOUND_MESSAGE
 } = require("./constants")
-const { directoryExists } = require("./helpers")
+const { courseUidList, directoryExists } = require("./helpers")
 const markdownGenerators = require("./markdown_generators")
 const loggers = require("./loggers")
 const helpers = require("./helpers")
@@ -33,12 +33,17 @@ const scanCourses = (inputPath, outputPath, jsonPath = null) => {
   }
   const courseList = jsonPath
     ? JSON.parse(fs.readFileSync(jsonPath))["courses"]
-    : fs.readdirSync(inputPath)
+    : fs.readdirSync(inputPath).filter(course => !course.startsWith("."))
   const numCourses = jsonPath
     ? courseList.length
     : courseList.filter(file => directoryExists(path.join(inputPath, file)))
       .length
   if (numCourses > 0) {
+    // populate the course uid mapping
+    courseList.forEach(async course => {
+      const courseUid = await getCourseUid(inputPath, course)
+      helpers.courseUidList[course] = courseUid
+    })
     console.log(`Converting ${numCourses} courses to Hugo markdown...`)
     progressBar.start(numCourses, 0)
     courseList.forEach(course =>
@@ -51,43 +56,55 @@ const scanCourses = (inputPath, outputPath, jsonPath = null) => {
   }
 }
 
+const getCourseUid = async (inputPath, course) => {
+  const coursePath = path.join(inputPath, course)
+  const masterJsonFile = await getMasterJsonFileName(coursePath)
+  if (masterJsonFile) {
+    const courseData = JSON.parse(fs.readFileSync(masterJsonFile))
+    return courseData["uid"]
+  }
+}
+
 const scanCourse = async (inputPath, outputPath, course) => {
   /*
     This function scans a course directory for a master json file and processes it
   */
+
   const coursePath = path.join(inputPath, course)
-  if (fs.lstatSync(coursePath).isDirectory()) {
-    if (helpers.directoryExists(coursePath)) {
-      // If the item is indeed a directory, read all files in it
-      const contents = await readdir(coursePath)
-      for (const file of contents) {
-        // If the item is a master json file, parse it and process into hugo markdown
-        if (
-          RegExp("^[0-9a-f]{32}_master.json").test(file) ||
-          file === "master.json"
-        ) {
-          const courseData = JSON.parse(
-            fs.readFileSync(path.join(coursePath, file))
-          )
-          const markdownData = markdownGenerators.generateMarkdownFromJson(
-            courseData
-          )
-          writeMarkdownFilesRecursive(
-            path.join(outputPath, courseData["short_url"]),
-            markdownData
-          )
-        }
-      }
-    } else {
-      const courseError = `${coursePath} - ${MISSING_COURSE_ERROR_MESSAGE}`
-      loggers.fileLogger.log({
-        level:   "error",
-        message: courseError
-      })
-      progressBar.increment()
-      throw new Error(courseError)
+  const masterJsonFile = await getMasterJsonFileName(coursePath)
+  if (masterJsonFile) {
+    const courseData = JSON.parse(fs.readFileSync(masterJsonFile))
+    const markdownData = markdownGenerators.generateMarkdownFromJson(courseData)
+    writeMarkdownFilesRecursive(
+      path.join(outputPath, courseData["short_url"]),
+      markdownData
+    )
+  }
+}
+
+const getMasterJsonFileName = async coursePath => {
+  /*
+    This function scans a course directory for a master json file and returns it
+  */
+  if (helpers.directoryExists(coursePath)) {
+    // If the item is indeed a directory, read all files in it
+    const contents = await readdir(coursePath)
+    const fileName = contents.find(
+      file =>
+        RegExp("^[0-9a-f]{32}_master.json").test(file) || file === "master.json"
+    )
+    if (fileName) {
+      return path.join(coursePath, fileName)
     }
   }
+  //  If we made it here, the master json file wasn't found
+  const courseError = `${coursePath} - ${MISSING_COURSE_ERROR_MESSAGE}`
+  loggers.fileLogger.log({
+    level:   "error",
+    message: courseError
+  })
+  progressBar.increment()
+  throw new Error(courseError)
 }
 
 const writeMarkdownFilesRecursive = (outputPath, markdownData) => {
@@ -139,5 +156,6 @@ const writeSectionFiles = (key, section, outputPath) => {
 module.exports = {
   scanCourses,
   scanCourse,
+  getMasterJsonFileName,
   writeMarkdownFilesRecursive
 }
