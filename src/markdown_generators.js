20c29c07922a854421111aa286f76021b05de94e
Carey Gumaer <gumaer.carey@gmail.com>
2020-08-26 11:17:56 -0400

    add turndown rule to handle inline code blocks and a helper function to handle pre-escaped backticks

--- src/markdown_generators.js
+++ src/markdown_generators.js
@@ -116,6 +116,35 @@ turndownService.addRule("codeblockfix", {
 })
 
 /**
+ * turn kbd, tt and samp elements into inline code blocks
+ */
+turndownService.addRule("kbdfix", {
+  filter: node =>
+    node.nodeName === "KBD" ||
+    node.nodeName === "TT" ||
+    node.nodeName === "SAMP",
+  replacement: (content, node, options) => {
+    /**
+     * fix weird formatting issue with some elements' content being wrapped
+     * between a backtick and a single quote
+     */
+    try {
+      const backTickSingleQuoteWrap = new RegExp(/(?<=\`)(.*?)(?=')/g)
+      content.match(backTickSingleQuoteWrap).forEach(match => {
+        content = content.replace(`\\\`${match}'`, match)
+      })
+      return `\`${content}\``
+    } catch (err) {
+      loggers.fileLogger.log({
+        level:   "error",
+        message: err
+      })
+    }
+    return content
+  }
+})
+
+/**
  * Build anchor link shortcodes
  **/
 turndownService.addRule("anchorshortcode", {
@@ -476,8 +505,8 @@ const generateCourseSectionMarkdown = (page, courseData) => {
     Generate markdown a given course section page
     */
   try {
-    return `${turndownService.turndown(
-      fixLinks(page["text"], page, courseData)
+    return `${helpers.unescapeBackticks(
+      turndownService.turndown(fixLinks(page["text"], page, courseData))
     )}${generateCourseFeaturesMarkdown(page, courseData)}`
   } catch (err) {
     loggers.fileLogger.log({
@@ -524,10 +553,14 @@ const generateCourseFeaturesMarkdown = (page, courseData) => {
           return {
             href:          fileName,
             "data-ngdesc": helpers.htmlSafeText(
-              turndownService.turndown(image["description"])
+              helpers.unescapeBackticks(
+                turndownService.turndown(image["description"])
+              )
             ),
             text: helpers.htmlSafeText(
-              turndownService.turndown(image["caption"])
+              helpers.unescapeBackticks(
+                turndownService.turndown(image["caption"])
+              )
             )
           }
         })
