9a863d2529d761592c548238c744d5fc57e9dad7
Carey Gumaer <gumaerc@mit.edu>
2020-09-18 11:56:16 -0400

    fmt

--- src/helpers_test.js
+++ src/helpers_test.js
@@ -203,9 +203,6 @@ describe("stripS3", () => {
   it("replaces the s3 prefix with a static prefix if set", () => {
     helpers.runOptions.staticPrefix = "/courses"
     const input = "https://open-learning-course-data.s3.amazonaws.com/test.jpg"
-    assert.equal(
-      helpers.stripS3(input),
-      "/courses/test.jpg"
-    )
+    assert.equal(helpers.stripS3(input), "/courses/test.jpg")
   })
 })
