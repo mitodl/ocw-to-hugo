790e84140d6a024cb2abf738441b88f35f9765a2
Carey Gumaer <gumaerc@mit.edu>
2021-03-26 13:50:43 -0400

    fmt

--- src/config_generators.js
+++ src/config_generators.js
@@ -2,7 +2,9 @@ const { getExternalLinks } = require("./helpers")
 
 const generateExternalLinksMenu = courseData => {
   return `${getExternalLinks(courseData).map((externalLink, index) => {
-    return `[[leftnav]]\n\tname = "${externalLink["title"]}"\n\turl = "${externalLink["url"]}"\n\tweight = ${index * 10 + 1000}`
+    return `[[leftnav]]\n\tname = "${externalLink["title"]}"\n\turl = "${
+      externalLink["url"]
+    }"\n\tweight = ${index * 10 + 1000}`
   })}`
 }
 
