76bb517601cdb3806546979351eeeaaef7017d98
Carey P Gumaer <gumaerc@mit.edu>
2021-05-17 10:28:18 -0400

    use deepEqual on the whole array instead of comparing each individual item

--- src/data_template_generators_test.js
+++ src/data_template_generators_test.js
@@ -134,12 +134,9 @@ describe("generateDataTemplate", () => {
   })
 
   it("sets the course_number property on the course data template to data parsed from sort_as and extra_course_number properties in the course json data", () => {
-    helpers
-      .getCourseNumbers(singleCourseJsonData)
-      .forEach((expectedValue, index) => {
-        const foundValue = courseDataTemplate["course_numbers"][index]
-        assert.equal(expectedValue, foundValue)
-      })
+    const expectedValues = helpers.getCourseNumbers(singleCourseJsonData)
+    const foundValues = courseDataTemplate["course_numbers"]
+    assert.deepEqual(expectedValues, foundValues)
   })
 
   it("sets the term property on the course data template to from_semester and from_year in the course json data", () => {
