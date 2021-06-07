b1afcccb0b796d2fbf68b29c44d06194176a7ae1
Carey Gumaer <gumaerc@mit.edu>
2021-06-07 15:25:43 -0400

    refactor generateExternalLinksMenu

--- src/config_generators.js
+++ src/config_generators.js
@@ -2,17 +2,14 @@ const yaml = require("js-yaml")
 
 const { getExternalLinks } = require("./helpers")
 
-const generateExternalLinksMenu = courseData => {
-  return yaml.safeDump({
-    leftnav: getExternalLinks(courseData).map((externalLink, index) => {
-      return {
-        name:   externalLink["title"],
-        url:    externalLink["url"],
-        weight: index * 10 + 1000
-      }
-    })
+const generateExternalLinksMenu = courseData =>
+  yaml.safeDump({
+    leftnav: getExternalLinks(courseData).map((externalLink, index) => ({
+      name:   externalLink["title"],
+      url:    externalLink["url"],
+      weight: index * 10 + 1000
+    }))
   })
-}
 
 module.exports = {
   generateExternalLinksMenu
